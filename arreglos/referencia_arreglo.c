33207e667ef58bce24868f80797e79f823761838
canaleija <robanac@gmail.com>
2024-10-24 07:55:08 -0600

    ordenamiento en funcion

--- arreglos/referencia_arreglo.c
+++ arreglos/referencia_arreglo.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-
-
 void modificarArreglo(int [], int);
+void imprimirArreglo(int [], int);
+void ordenarBurbuja(int [], int);
 
-int main(int argc, char const *argv[])
+int main()
 {
     int arreglo [5] = {2,5,89,7,5};
-    modificarArreglo(arreglo,5); 
-    int x;
-    for(x=0;x<5;x++){
-        printf("%d\n",arreglo[x]);
-    }
+    int arreglo2 [10] = {2,4,1,1,2,3,4,3,289,5};
+    ordenarBurbuja(arreglo, 5);
+    imprimirArreglo(arreglo,5);
+    ordenarBurbuja(arreglo2,10);
+    imprimirArreglo(arreglo2,10);
     return 0;
 }
 
@@ -22,5 +22,29 @@ for(x=0; x<tam; x++){
 
     b[x]*=2;
 }
+
+}
+
+void imprimirArreglo(int arreglo[], int tam){
+    int x;
+    for(x=0;x<tam;x++){
+        printf("%d\n",arreglo[x]);
+    }
+}
+
+void ordenarBurbuja(int x [], int tam){
+int y,i,j;
+
+    // burbuja
+    for(i=1; i<tam;i++){
+        for(j=0; j<tam-1;j++){
+            if(x[j]>x[j+1]){
+                int temp = x[j];
+                x[j] = x[j+1];
+                x[j+1] = temp;
+            }
+        }
+    }
+
 
 }
